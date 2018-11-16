5ad09b060ee787df9a032d341b2de13ac1140a02
SB <shannon.marie.bailey@gmail.com>
2018-11-15 23:01:37 -0600

    Implemented unit test for StationsRepositoryController

---
+++ stations/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into stations
+values('iheart101', 'iheartCountry',true);
+
+insert into stations
+values('iheart102', 'iheartRock',false);
---
+++ stations/src/test/resources/schema.sql
@@ -0,0 +1,9 @@
+drop table if exists stations;
+create table stations
+(
+   id integer not null,
+   station_id varchar(255) not null,
+   station_name varchar(255) not null,
+   hd_enabled bool not null,
+   primary key(id)
+);
